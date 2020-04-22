2f3b35f9f6102df1ae1229d0278e11fc25e7e048
Juliana Resplande <julianarsg13@gmail.com>
2020-04-21 23:20:04 -0300

    organize code

--- extract.py
+++ extract.py
@@ -1,80 +1,157 @@
-from transformers import AutoModel, AutoTokenizer
+"""Extractor for TensorFlow Embbeding Projector  
+
+Embbeding are extracted from text using HuggingFace Transformers.
+Vocabulary is saved as vocab.tsv.
+Embbedings vectors are saved as embedding.tsv.
+"""
+
 from enum import Enum
+from transformers import AutoModel, AutoTokenizer
 import pandas as pd
 import nltk
 import torch
 
 class Level(Enum):
+    """ Input text segmentation levels
+    """
+
     token = 1
     word = 2
     sentence = 3
 
-def vocabulary_and_embeddings(text, tokenizer, model, level, aggr_func):    
+def form_token_embeddings(text, tokenizer, model):
+    """Generate tokense embeddings vector
+
+        Args:  
+            text (str): input text  
+            tokenizer (transformers.AutoTokenizer): HuggingFace Tokenizer  
+            model (transformers.AutoModel): HuggingFace Transformer Model  
+
+       Returns:  
+            embeddings: tokens embeddings vector
+    """
+    encoded = tokenizer.encode(text, add_special_tokens=False)
+    input_ids = torch.tensor(encoded).unsqueeze(0)
+
+    return model(input_ids)[0][0]
+
+
+def form_word_embeddings(text, words, tokenizer, model, aggr_func):
+    """Generate tokense embeddings vector
+
+        Args:  
+            text (str): input text  
+            words (str): word list  
+            tokenizer (transformers.AutoTokenizer): HuggingFace Tokenizer  
+            model (transformers.AutoModel): HuggingFace Transformer Model  
+            aggr_func (func): Agreggation Function to form embedding  
+
+       Returns:  
+            embeddings: tokens embeddings vector
+    """
+
+    tokens, token_embeddings = vocabulary_and_embeddings(text, tokenizer, model, 'token', None)
+
+    token_idx = 0
+    map_idx = list()
+    for idx, word in enumerate(words):
+        token = tokens[token_idx]
+
+        if word == tokens[token_idx]:
+            map_idx.append(idx)
+        else:
+            word_idx = list()
+            while token != word:
+                token_idx = token_idx+1
+                token += tokens[token_idx].replace('#', "")
+                word_idx.append(token_idx)
+            map_idx.append(word_idx)
+        token_idx = token_idx+1
+
+    word_embeddings = list()
+    for idx in map_idx:
+        token = token_embeddings[idx]
+        word_embedding = token_embeddings[idx] if isinstance(idx) == int \
+                         else aggr_func(torch.stack(token), 0)
+        word_embeddings.append(word_embedding.tolist())
+    return word_embeddings
+
+def form_sentence_embeddings(sentences, tokenizer, model, aggr_func):
+    """Generate sentence embeddings vector  
+
+        Args:  
+            sentences (str): sentences list  
+            tokenizer (transformers.AutoTokenizer): HuggingFace Tokenizer  
+            model (transformers.AutoModel): HuggingFace Transformer Model  
+            aggr_func (func): Agreggation Function to form embedding  
+            
+       Returns:  
+            embeddings: sentences embeddings vector
+    """
+    sentence_embeddings = list()
+    for sentence in sentences:
+        token_embeddings = form_token_embeddings(sentence, tokenizer, model)
+        sentence_embeddings.append(aggr_func(token_embeddings, 0).tolist())
+
+    return  sentence_embeddings
+
+def vocabulary_and_embeddings(text, tokenizer, model, level, aggr_func):
+    """Generate embeddings vector and vocabulary  
+
+        Args:  
+            text (str): input text  
+            tokenizer (transformers.AutoTokenizer): HuggingFace Tokenizer  
+            model (transformers.AutoModel): HuggingFace Transformer Model  
+            level (str): Segmentation level  
+            aggr_func (func): Agreggation Function  
+
+       Returns:  
+            vocabulary: embeddings vocabulary  
+            embeddings: embeddings vector
+    """
+
     nltk.download('punkt')
 
-    if level in ['token', 'word']:
-        encoded = tokenizer.encode(text,add_special_tokens=False) #don't use [CLS] and [SEP]
-        input_ids = torch.tensor(encoded).unsqueeze(0)
-
+    if level == 'token':
         vocab = tokenizer.tokenize(text)
-        embeddings = model(input_ids)[0][0]
-        
-        if level == 'token':
-            return vocab, embeddings.tolist()
-        elif level == 'word':
-            words = nltk.word_tokenize(text)
+        embeddings = form_token_embeddings(text, tokenizer, model)
+    elif level == 'word':
+        vocab = nltk.word_tokenize(text)
+        embeddings = form_word_embeddings(text, vocab, tokenizer, model, aggr_func)
+    elif level == 'sentenc':
+        vocab = nltk.sent_tokenize(text)
+        embeddings = form_sentence_embeddings(vocab, tokenizer, model, aggr_func)
 
-            vocab_idx = 0
-            map_idx = []
-            selected_words = []
-            for idx, word in enumerate(words):
-                if word == vocab[vocab_idx]:
-                    if len(word) > 1: #ignore punctuations
-                        map_idx.append(idx)
-                        selected_words.append(word)
-                else:
-                    word_idx = []
-                    term = vocab[vocab_idx]      
-                    while(word != term):
-                        vocab_idx = vocab_idx+1
-                        term+= vocab[vocab_idx].replace('#', "")
-                        word_idx.append(vocab_idx)
-                    map_idx.append(word_idx)
-                    selected_words.append(word)
-                
-                vocab_idx = vocab_idx+1
-            selected_embeddings = []
-            for idx in map_idx:
-                if type(idx) == int:
-                    selected_embeddings.append(embeddings[idx].tolist())
-                elif type(idx) == list:
-                    word_embbedings = [embeddings[i] for i in idx]
-                    mean = aggr_func(torch.stack(word_embbedings), 0)
-                    selected_embeddings.append(mean.tolist())
+
+    return vocab, embeddings
+
+def extract_embeddings(text, model_name, level_name, aggr_func=torch.mean):
+    """Extract embeddings from text using Hugging Face model
+
+        Embbedings are save as embeddings.tsv.
+        Vocabulary is saved as vocabulary.tsv.
+
             
-            return  selected_words, selected_embeddings
-    elif level == 'sentence':
-        sentences = nltk.sent_tokenize(text)
-        embeddings_list = []
-        for sentence in sentences:
-            encoded = tokenizer.encode(sentence,add_special_tokens=False) #don't use [CLS] and [SEP]
-            input_ids = torch.tensor(encoded).unsqueeze(0)
-            embeddings = model(input_ids)[0][0]
+        Args:  
+            text (str): input text  
+            model_name (str): Hugging Face model name  
+            level_name (str): Hugging Face model  
+            aggr_func (func): Agreggation Function  
 
-            embeddings_list.append(aggr_func(embeddings, 0).tolist())
+       Raises:  
+            ValueError: f'There is no {level_name} segmentation.'
+    """
 
-        return sentences, embeddings_list
-
-def extract_embeddings(text, model_name, level_name, aggr_func = torch.mean):    
-    assert Level[level_name]
+    if level_name not in Level:
+        raise ValueError(f'There is no {level_name} segmentation.')
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModel.from_pretrained(model_name)
-   
-    vocab, embeddings = vocabulary_and_embeddings(text, tokenizer, model, level_name, aggr_func)   
 
-    df = pd.DataFrame(embeddings) 
-    df.to_csv('embeddings.tsv', index = None, sep = '\t', header = None)
+    vocab, embeddings = vocabulary_and_embeddings(text, tokenizer, model, level_name, aggr_func)
+
+    df = pd.DataFrame(embeddings)
+    df.to_csv('embeddings.tsv', index=None, sep='\t', header=None)
 
     series = pd.Series(vocab)
-    series.to_csv('vocab.tsv', index = None, sep = '\n', header = None)
+    series.to_csv('vocab.tsv', index=None, sep='\n', header=None)
